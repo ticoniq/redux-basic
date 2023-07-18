c866cf425a283c138ff95f576c20d4f2835e6e16
ticoniq_ <okolietochi@gmail>
2023-07-18 07:49:19 +0100

    create cart

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems';
+
+const initialState = {
+  cartItems : cartItems,
+  amount: 4,
+  total: 0,
+  isLoading: true
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      console.log(action)
+    }
+  },
+});
+
+// console.log(cartSlice)
+export const { clearCart } = cartSlice.actions;
+export const { removeItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
